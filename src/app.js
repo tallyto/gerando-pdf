83c1ca466a387732c43308a14485a18c93249d4b
Tallyto Rodrigues <otallyto@MacBook-Pro-de-Tallyto.local>
2020-06-23 22:19:50 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const pdf = require("html-pdf");
+const ejs = require("ejs");
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  ejs.renderFile(
+    "./templates/index.ejs",
+    { name: "Agil - Render PDF" },
+    (error, html) => {
+      if (error) {
+        return res.status(500).json({ message: "Error in Server" });
+      }
+      const options = {
+        format: "A4",
+        border: {
+          right: "8",
+        },
+      };
+      pdf.create(html, options).toFile("./uploads/report.pdf", (error, response) => {
+        if (!error) {
+          return res.json({ message: "PDF Genereted" });
+        } else {
+          return res.status(500).json({ message: "Fail in Generated PDF" });
+        }
+      });
+    }
+  )
+
+  
+});
+
+app.listen(3333);
